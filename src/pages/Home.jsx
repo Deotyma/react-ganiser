73453c7f7b6fa5f89e234cbaf25bcc82eb35b4c1
Ewa Kadziolka <kalistaewa@gmail.com>
2022-03-08 13:46:50 +0100

    first home ui

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+      <div className="container">
+       <h1 className="welcomeH"> Welcome to our app</h1>
+       <div className="buttons">
+          <button className="primary_button">Log in</button>
+          <button className="primary_button">Sign up</button>
+       </div>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
